447f4ef1cd47e6fcbadbc9fea3bb828a0978909c
Ivana <i.krivcevska@gmail.com>
2019-03-24 20:46:39 +0100

    Add new fragment 'InsertWordFragment'

---
+++ app/src/main/java/com/example/mydictionary/InsertWordFragment.java
@@ -0,0 +1,29 @@
+package com.example.mydictionary;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InsertWordFragment extends Fragment {
+
+
+    public InsertWordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert_word, container, false);
+    }
+
+}
