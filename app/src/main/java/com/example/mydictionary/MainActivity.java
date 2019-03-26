b14c63dae80c65ef6e0a404b949f7f8ba36e7f0b
Ivana <i.krivcevska@gmail.com>
2019-03-26 14:24:27 +0100

    Add different search layout for landscape orientation

--- app/src/main/java/com/example/mydictionary/MainActivity.java
+++ app/src/main/java/com/example/mydictionary/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.mydictionary;
 
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragment();
 
-        SearchFragment sf = new SearchFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, sf).commit();
+
+    }
+
+    private void addFragment() {
+        FragmentManager manager = getSupportFragmentManager();
+
+        SearchFragment searchFragment = new SearchFragment();
+
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // In portrait
+//            Fragment f = manager.findFragmentById(R.id.landscape);
+//            if(f!=null)
+//            {
+//                manager.beginTransaction().remove(f).commit();
+//            }
+            getSupportFragmentManager().beginTransaction().add(R.id.container, searchFragment).commit();
+        } else {
+            // In landscape
+//            Fragment f = manager.findFragmentById(R.id.portrait);
+//            if(f!=null)
+//            {
+//                manager.beginTransaction().remove(f).commit();
+//            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+        }
     }
 
 }
